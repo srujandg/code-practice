b841963b4ebd9b1f9fb094e0e1ae0152227fcb8d
Srujan <srujansanju870@gmail.com>
2026-01-24 20:15:38 +0530

    Create Variables.java

---
+++ Variables.java
@@ -0,0 +1,11 @@
+// Learnt variables in java and how it works in java
+
+class Variables{
+  public static void main( String args [])
+  { 
+    int a=10;
+    int b=20;
+    System.out.println(a+b);
+  }
+}
+    
