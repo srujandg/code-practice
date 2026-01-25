9ce54662a4a204c01ebec1126cf8e684db0c8674
Srujan <srujansanju870@gmail.com>
2026-01-25 13:50:31 +0530

    Add Variables class to demonstrate addition
    Added a new class to demonstrate variable usage and addition in Java.

--- Variables.java
+++ Variables.java
@@ -8,4 +8,13 @@ class Variables{
     System.out.println(a+b);
   }
 }
-    
+
+// Program to demonstrate variables and addition in java
+class Variables{
+  public static void main ( String args[] )
+  {
+    int num1=10;
+    int num2=20;
+    int sum=num1+num2;
+      System.out.println("sum="+sum);
+  }}
