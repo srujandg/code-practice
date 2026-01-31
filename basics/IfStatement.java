5b0e27dc8c683e5041f8b85a1aa2a0f11a1ae1eb
Srujan <srujansanju870@gmail.com>
2026-01-31 20:21:45 +0530

    Practice if-else conditions for marks grading

---
+++ basics/IfStatement.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int marks=98;
+         if(marks>=0&&marks<=35)
+         System.out.println("Fail");
+          else if(marks>=36&&marks<=60)
+         System.out.println("B");
+         else if(marks>=61&&marks<=90)
+         System.out.println("A");
+          else if(marks>=91&&marks<=100)
+         System.out.println("A+");
+}}
