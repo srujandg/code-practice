a84a26a35b92b9d62e37991ed72f8fcf34f43b65
Srujan <srujansanju870@gmail.com>
2026-01-31 20:04:16 +0530

    Learnt Switch Statement in Java

---
+++ basics/Switch.java
@@ -0,0 +1,19 @@
+
+class Main {
+    public static void main(String[] args) {
+        int n=0;
+        switch(n)
+        {
+        case 0:
+            System.out.println("I am Case 0");
+            break;
+        
+    case 1:
+            System.out.println("I am Case 1");
+            break;
+    
+    case 2:
+        System.out.println("I am Case 2");
+        break;
+    }
+}}
